131ff4fb3cadd2452c247ec575d333cff834c367
nfiege <uk025743@student.uni-kassel.de>
2021-09-17 17:59:41 +0200

    added directories

--- scripts/generate_directories/generate_directories.sh
+++ scripts/generate_directories/generate_directories.sh
@@ -4,3 +4,11 @@ mkdir ../../synth_results
 mkdir ../../synth_results/energy_plot
 mkdir ../../synth_results/synth_raw
 mkdir ../../synth_results/synth_raw/split
+
+mkdir ../../rat_II_synth/mat_inv
+mkdir ../../rat_II_synth/mat_inv/UniformILPNew
+mkdir ../../rat_II_synth/mat_inv/NonUniformILP
+mkdir ../../rat_II_synth/r2_FFT
+mkdir ../../rat_II_synth/r2_FFT/UniformILPNew
+mkdir ../../rat_II_synth/r2_FFT/NonUniformILP
+mkdir ../../int_II_synth/r2_FFT
